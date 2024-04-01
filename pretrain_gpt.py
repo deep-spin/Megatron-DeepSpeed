202caed32d9dda75cd92118ffb315eb4527d2207
Ben Peters <bpeters@coli.uni-saarland.de>
2024-04-01 14:38:31 +0100

    slightly more informative support logging

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -231,12 +231,12 @@ def loss_func(loss_mask, moe_loss, mos_loss, support_size, output_tensor):
         if max(args.num_experts) <= 1:
             if support_size is not None:
                 support_size = support_size.float()
-                # need to average the support sizes, I guess
-                # todo: return support
-                # average_support = average_losses_across_data_parallel_group([support_size])
                 torch.distributed.all_reduce(support_size, group=mpu.get_data_parallel_group())
                 support_size = support_size / torch.distributed.get_world_size(group=mpu.get_data_parallel_group())
-                return loss, {'lm loss': averaged_loss[0], 'support size': support_size.mean()}
+                # this way to compute the max support will be incorrect on
+                # multiple GPUs, but the numbers should still be reasonably
+                # informative
+                return loss, {'lm loss': averaged_loss[0], 'support size': support_size.mean(), "support max": support_size.max()}
             else:
                 return loss, {'lm loss': averaged_loss[0]}
         else:
